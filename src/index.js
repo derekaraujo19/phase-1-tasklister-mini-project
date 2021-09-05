25201940f2a9fa82fe0a93b2d8d35aabfaa4cc38
Derek Araujo <derekaraujo19@gmail.com>
2021-09-05 19:43:25 -0400

    assignment complete

--- src/index.js
+++ src/index.js
@@ -1,3 +1,33 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  // grab form, add event listener w/ a submit event
+    // add a prevent default
+    // event listener will point to an external function to append task to DOM
+
+  // createTask function createst list element
+    // list's text content = passed in text from event handler
+    // append's list to UL
+
+
+  function createTask (task) {
+    let li = document.createElement('li')
+    let btn = document.createElement('button')
+    btn.addEventListener('click', deleteTask)
+    btn.textContent = 'Done!'
+    li.textContent = task + ' ';
+    li.appendChild(btn);
+    document.getElementById('tasks').appendChild(li);
+  }
+
+  function deleteTask (e) {
+    e.target.parentNode.remove()
+  }
+
+
+  document.querySelector('form').addEventListener('submit', (e) => {
+    e.preventDefault()
+    const textBox = document.getElementById('new-task-description');
+    createTask(textBox.value);
+    document.querySelector('form').reset();
+  })
+
 });
